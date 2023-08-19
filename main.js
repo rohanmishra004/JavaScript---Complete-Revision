be1919dacddea5a7ece42d4d5f8e36c30b89e2d1
Rohan Mishra <rohanmishra.004@gmail.com>
2023-08-19 13:43:12 +0530

    Closures with IIFE

--- main.js
+++ main.js
@@ -1,32 +1,39 @@
-// Lexical Scope Defines how variable names are resolved in nested function , ex: a child function inside a parent function would have access to parent function variables along with global variables
+//IIFE - Imediately Invoked function Expression
+
+// const counter = (() => {
+//     let count = 0;
+//     console.log(`Initial Value :${ count }`)
+//     return () => { count += 1; console.log(count) }
+// })();
 
 
-//a closure is a function having access to the parent scope even after the parent function is closed
+// counter()
+// counter()
 
-//global scope - 
 
-let x = 1;
 
-const parent = () => {
-    //local scope
-    let myVal = 2;
 
-    //child function
+const credit = ((num) => {
+    let credit = num;
+    console.log(`Initial Credit::${ credit }`)
+    return (() => {
+        credit -= 1;
+        if (credit != 0) {
+            console.log(`Playing game::${ credit }`);
+        }
+        if (credit === 0) {
+            console.log(`Add more credit to play`)
+        }
+    })
+})(3)
 
-    const child = () => {
-        console.log(x,myVal+1)
-    }
-    //in order to ensure that child function forms closure with parent and global , instead of calling we are only returning a reference of the function
-    return child
-}   
+credit()
+credit()
+credit()
 
-//if we try to access my myVal from outside the function we will get reference error
-// child()
 
-const res = parent();
 
-console.log(res)
 
-//Although the parent function is closed the child function still has reference to the lexical scope of the parent
-res();
+
+
 
