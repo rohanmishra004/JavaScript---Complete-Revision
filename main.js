a485e8d97742b756c304bcbf2b4704d30209b8ef
Rohan Mishra <rohanmishra.004@gmail.com>
2023-08-22 19:35:24 +0530

    Decorators examples

--- main.js
+++ main.js
@@ -1,130 +1,96 @@
-// //Official Def of Recursion
-// // In CS recursion is the method to solve a problem by solving smaller instances of the same problem
+//JS 
 
+// Decorators function weap a function into another function
 
-
-// //Any interator function can be written as Recursive 
-
-// const counter=(num)=>{
-//     while (num > 0){
-//         console.log(num);
-//         num-=1
-//     }
-// }
-
-// // counter(10)
-
-// //Recursive functions have 2 calls - 
-// // 1 - Recursive call to the function 
-// // 2 - at least one conditon to exit
-
-// const recurCounter = (num) => {
-
-//     //exit condition
-//     if (num < 0) {
-//         return
-//     }
-//     console.log(num);
-//     num -= 1
-//     //recursive call
-//     recurCounter(num)
-// }
-
-// // recurCounter(11)
-
-// // --------------------------------------------------------
-
-
-// //Standard example of recursion sample codes
-
-
-// //Without recursion fibonacci
-
-// const withoutRecurFibo = (num, array=[0,1]) => {
-//     while (num > 2){
-//         const [nextToLast, Last] = array.slice(-2);
-//         array.push(nextToLast+ Last);
-//         num-=1
-//     }
-
-//     return array
-//     // return array[num]
-// }
-
-// // console.log(withoutRecurFibo(12))
-
-// //Fibo with Recur
-
-// const withRecurFibo = (num, array=[0,1]) => {
-//     if (num <= 2) return array;
-//     const [nextToLast, Last] = array.slice(-2);
-//     console.log([...array])
-//     console.log(nextToLast,Last)
-//     return withRecurFibo(num-1, [...array, nextToLast+Last])
-// }
-
-// // console.log(withRecurFibo(12))
-
-
-// //Return the nth position of the fibonacci sequence
-
-// // Without recur
-
-// const withoutRecur = (pos) => {
-//     if (pos <= 1) return pos;
-//     const seq = [0, 1]
-//     for (let i = 2; i <= pos; i++){
-//         const [nextToLast, last] = seq.slice(-2);
-//         seq.push(nextToLast + last);
-//     }
-
-//     return seq[pos];
-// }
-
-// // console.log(withoutRecur(6))
-
-
-// //With Recursion
-
-// // const withRecur = (pos) => {
-// //     if (pos <= 1) return pos;
-// //     //Test code
-// //     console.log(withRecur(pos-1))
-// //     console.log(withRecur(pos-2))
-// //     return withRecur(pos-1)+withRecur(pos-2)
-// // }
-
-// const withRecur = pos => pos<2?pos:withRecur(pos-1)+withRecur(pos-2)
-
-
-// console.log(withRecur(7))
+//These wrappers 'decorate' the original function with new capabilities
 
 
 
 
-const artistsList = {
-    jazz: ["miles ", "John"],
-    rock: {
-        classics: ["Bob Seager", 'The Eagles'],
-        alt: {
-            classic:["Pearl", "The Kiler"]
-        }
-    },
-    unclassified: {
-        new:["Caamp","Niel Young"]
+//Ex -1 - Using closures to log how many times a function is called 
+
+let sum = (...args) => {
+    return [...args].reduce((acc,num)=>acc+num)
+}
+
+const callCounter = (fn) => {
+    let count = 0;
+
+    return (...args) => {
+        console.log(`Sum has been called ${count += 1 } times`)
+        return fn(...args)
     }
 }
 
-const getArtistsName = (dataObj, arr = []) => {
-    Object.keys(dataObj).forEach(key => {
-        if (Array.isArray(dataObj[key])) {
-            return dataObj[key].forEach(artist=> {
-                arr.push(artist)
-            })
-        }
-        getArtistsName(dataObj[key],arr)
-    })
-    return arr
+// sum = callCounter(sum)
+// console.log(sum(1,2,4,5))
+// console.log(sum(1,2,5))
+
+
+//Example 2 - Check for valid data and number of params
+let rectArea = (length, width) => {
+    return length*width
 }
 
-console.log(getArtistsName(artistsList))
+
+const countParams = (fn) => {
+    return (...args) => {
+        if (args.length !== fn.length) {
+            throw new Error('Incorrrct number of params ', fn.name)
+        }
+
+        return fn(...args)
+    }
+}
+
+
+const reqInteger = (fn) => {
+    return (...params) => {
+        params.forEach(param => {
+            if (!Number.isInteger(param)) {
+                throw new Error(`Params for ${fn.name} should be integer`)
+            }
+        })
+        return fn(...params)
+    }
+}
+
+
+// rectArea = countParams(rectArea)
+// rectArea = reqInteger(rectArea)
+
+// console.log(rectArea(10,"20"))
+
+
+
+//Example 3 - Decorating API calls functions 
+//Time data requests during developement
+let url = "https://jsonplaceholder.typicode.com/todos/42"
+let reqData = async (url) => {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+
+let dataResonseTime = (fn) => {
+    return async (url) => {
+        console.time('fn');
+        const data = await fn(url);
+        console.timeEnd('fn');
+        return data
+    }
+}
+
+
+const testFunction = async () => {
+    reqData = dataResonseTime(reqData);
+    const data = await reqData(url);
+    console.log(data)
+}
+
+testFunction()
